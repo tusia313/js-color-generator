ae28b3092296539d0d961dfd4de35ff2a56e2a65
Marta Łoskot <loskotmarta@gmail.com>
2023-10-18 13:19:12 +0200

    Add responsivness to the page.

--- js/app.js
+++ js/app.js
@@ -1,25 +1,22 @@
 const button = document.querySelector('button')
 const colorContainers = document.querySelectorAll('.color-container')
 
-for (const i = 0; i < 4; i++) {
-    for (let i = 0; i < 3; i++) {
-        let randomColor = []
-        let randomNumber = Math.ceil(Math.random() * 255)
-        randomColor.unshift(randomNumber)
-        console.log(randomColor)
-    }
+let randomColor = []
+function createRgb(e) {
+for (let i = 0; i < 3; i++) {
+    let randomNumber = Math.ceil(Math.random() * 255)
+    randomColor.unshift(randomNumber)
+}
+return randomColor
 }
 
+// function changeNumbers(e) {
+//     console.log(e.key)
+//     if (e.key === 32) {
 
+//     }
+// }
 
-function changeNumbers(e) {
-    console.log(e.key)
-    if (e.key === 32) {
-
-    }
-
-}
-
-button.addEventListener('click', changeNumbers)
-document.addEventListener('keydown', changeNumbers)
+button.addEventListener('click', createRgb)
+document.addEventListener('keydown', createRgb)
 
