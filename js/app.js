32459199eae79576d5839af047bf85f6e6d39f07
Marta Łoskot <loskotmarta@gmail.com>
2024-02-07 12:44:17 +0100

    Trying to write new js code

--- js/app.js
+++ js/app.js
@@ -1,34 +1,49 @@
 const spawnButton = document.querySelector('#spawn-button')
 const colorsContainer = document.querySelector('.colors-container')
 
-function getRandomNumber() {
-    let randomNumber = Math.floor(Math.random() * 255)
-    return randomNumber
-}
+// skupiamy się na rgb(liczba1, liczba2, liczba3), gdzie stała ma być tylko liczba1 dla każdego z 4 kolorów, a pozostałe dwie to obojętne
 
 let colorBlocks = ['', '', '', '']
+function spawn() {
+    console.log('spawning !')
 
-function makeColors() {
-    // dzięki temu nie dzielą się nam te kolory w nieskończoność !
-    colorsContainer.innerHTML = ''
-    const commonColor = getRandomNumber()
-    console.log('commonColor ' + commonColor)
-
-    colorBlocks.forEach((_colorBlock, index) => {
-        // uwaga, nie ma nawiasów przed treścią ('rgb')!
-        colorBlocks[index] = 'rgb(' +  commonColor + ',' + getRandomNumber() + ',' + getRandomNumber() + ')'
-    })
-    console.log(colorBlocks)
-
-    colorBlocks.forEach(colorBlock => {
-        const divElement = document.createElement('div')
-        // in order to see what i am doing : textContent
-        divElement.textContent = colorBlock
-        divElement.style.backgroundColor = colorBlock
-        colorsContainer.append(divElement)
+    colorBlocks.forEach( colorBlock => {
+       const divElement = document.createElement('div')
     })
 }
-makeColors()
-spawnButton.addEventListener('click', makeColors)
-document.addEventListener('keydown', makeColors)
+
+
+spawnButton.addEventListener('click', spawn)
+document.addEventListener('keydown', spawn)
+
+// function getRandomNumber() {
+//     let randomNumber = Math.floor(Math.random() * 255)
+//     return randomNumber
+// }
+
+// let colorBlocks = ['', '', '', '']
+
+// function makeColors() {
+//     // dzięki temu nie dzielą się nam te kolory w nieskończoność !
+//     colorsContainer.innerHTML = ''
+//     const commonColor = getRandomNumber()
+//     console.log('commonColor ' + commonColor)
+
+//     colorBlocks.forEach((_colorBlock, index) => {
+//         // uwaga, nie ma nawiasów przed treścią ('rgb')!
+//         colorBlocks[index] = 'rgb(' +  commonColor + ',' + getRandomNumber() + ',' + getRandomNumber() + ')'
+//     })
+//     console.log(colorBlocks)
+
+//     colorBlocks.forEach(colorBlock => {
+//         const divElement = document.createElement('div')
+//         // in order to see what i am doing : textContent
+//         divElement.textContent = colorBlock
+//         divElement.style.backgroundColor = colorBlock
+//         colorsContainer.append(divElement)
+//     })
+// }
+// makeColors()
+// spawnButton.addEventListener('click', makeColors)
+// document.addEventListener('keydown', makeColors)
 
