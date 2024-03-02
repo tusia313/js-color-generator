7ab7fab5cc25a3c769bccf91145257e95151f637
Marta Łoskot <loskotmarta@gmail.com>
2024-03-02 21:00:27 +0100

    Creating color blocks

--- js/app.js
+++ js/app.js
@@ -1,49 +1,18 @@
-const spawnButton = document.querySelector('#spawn-button')
-const colorsContainer = document.querySelector('.colors-container')
+const colorsContainers = document.querySelector(".colors-container")
+const spawnButton = document.querySelector("#spawn-button")
 
-// skupiamy się na rgb(liczba1, liczba2, liczba3), gdzie stała ma być tylko liczba1 dla każdego z 4 kolorów, a pozostałe dwie to obojętne
+let colorBlocks = [ 'a', 'b', 'c', 'd']
 
-let colorBlocks = ['', '', '', '']
-function spawn() {
-    console.log('spawning !')
+colorBlocks.forEach(colorBlock => {
+   const divElement = document.createElement('div')
+   divElement.textContent = colorBlock
+   colorsContainers.append(divElement)
+})
 
-    colorBlocks.forEach( colorBlock => {
-       const divElement = document.createElement('div')
-    })
+
+function spawn(){
+    console.log('spawn')
 }
-
-
 spawnButton.addEventListener('click', spawn)
 document.addEventListener('keydown', spawn)
-
-// function getRandomNumber() {
-//     let randomNumber = Math.floor(Math.random() * 255)
-//     return randomNumber
-// }
-
-// let colorBlocks = ['', '', '', '']
-
-// function makeColors() {
-//     // dzięki temu nie dzielą się nam te kolory w nieskończoność !
-//     colorsContainer.innerHTML = ''
-//     const commonColor = getRandomNumber()
-//     console.log('commonColor ' + commonColor)
-
-//     colorBlocks.forEach((_colorBlock, index) => {
-//         // uwaga, nie ma nawiasów przed treścią ('rgb')!
-//         colorBlocks[index] = 'rgb(' +  commonColor + ',' + getRandomNumber() + ',' + getRandomNumber() + ')'
-//     })
-//     console.log(colorBlocks)
-
-//     colorBlocks.forEach(colorBlock => {
-//         const divElement = document.createElement('div')
-//         // in order to see what i am doing : textContent
-//         divElement.textContent = colorBlock
-//         divElement.style.backgroundColor = colorBlock
-//         colorsContainer.append(divElement)
-//     })
-// }
-// makeColors()
-// spawnButton.addEventListener('click', makeColors)
-// document.addEventListener('keydown', makeColors)
 
