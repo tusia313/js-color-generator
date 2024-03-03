846507657f107ae8ae96bb0c0cbe3f2d04434bf7
Marta Łoskot <loskotmarta@gmail.com>
2024-03-03 21:08:13 +0100

    Generating random colors

--- js/app.js
+++ js/app.js
@@ -1,18 +1,32 @@
 const colorsContainers = document.querySelector(".colors-container")
 const spawnButton = document.querySelector("#spawn-button")
 
-let colorBlocks = [ 'a', 'b', 'c', 'd']
+let colorBlocks = [ '', '', '', '']
 
-colorBlocks.forEach(colorBlock => {
-   const divElement = document.createElement('div')
-   divElement.textContent = colorBlock
-   colorsContainers.append(divElement)
-})
-
+function getRandomNumber(){
+    return Math.floor(Math.random() * 255)
+}
 
 function spawn(){
     console.log('spawn')
+
+    const commonColor = getRandomNumber()
+    console.log(commonColor)
+
+// _ oznacza, że nie będziemy uzywac colorBlocków tylko raczej indexu w nim (itself)
+    colorBlocks.forEach((_colorBlock, index) => {
+        colorBlocks[index] = 
+        'rgb(' + commonColor + ','+ getRandomNumber() + ',' + getRandomNumber() +')'
+    })   
+
+colorBlocks.forEach(colorBlock => {
+    const divElement = document.createElement('div')
+    divElement.textContent = colorBlock
+    colorsContainers.append(divElement)
+ })
 }
+
+
 spawnButton.addEventListener('click', spawn)
 document.addEventListener('keydown', spawn)
 
