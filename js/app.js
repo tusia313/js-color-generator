2c1d36090fa2aa11eab659f06eec155606eab31c
Marta Łoskot <loskotmarta@gmail.com>
2024-03-04 09:45:54 +0100

    Finish it

--- js/app.js
+++ js/app.js
@@ -8,10 +8,11 @@ function getRandomNumber(){
 }
 
 function spawn(){
-    console.log('spawn')
+    // to nam czysci kontener z poprzednich kolorów, bo bysmy ich mieli nieskoncznie wiele
+    colorsContainers.innerHTML = ''
 
     const commonColor = getRandomNumber()
-    console.log(commonColor)
+  
 
 // _ oznacza, że nie będziemy uzywac colorBlocków tylko raczej indexu w nim (itself)
     colorBlocks.forEach((_colorBlock, index) => {
@@ -21,12 +22,13 @@ function spawn(){
 
 colorBlocks.forEach(colorBlock => {
     const divElement = document.createElement('div')
+    divElement.style.backgroundColor = colorBlock
     divElement.textContent = colorBlock
     colorsContainers.append(divElement)
  })
 }
-
-
+// wywołuje funckje zeby na wstępie coś było
+spawn()
 spawnButton.addEventListener('click', spawn)
 document.addEventListener('keydown', spawn)
 
